b854fbcb712933bcc67a2d0e6dc49aadef382956
SurweeshSP <surweeshsp@gmail.com>
2025-07-10 17:58:55 +0530

    Updated limiters

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,8 +1,8 @@
-import ratelimit from "../config/upslash.js";
+import rateLimit from "../config/upstash.js";
 
 const rateLimiter = async(req,res,next)=>{
     try{
-        const{success} = await ratelimit.limit("my-rate-limit");
+        const{success} = await rateLimit.limit("my-rate-limit");
         if(!success){
             return res.status(429).json({
                 message:"too many req, pls try later!!!"
